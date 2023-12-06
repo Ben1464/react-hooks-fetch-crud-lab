fdf0c03c684c2124fa9960d124a0d5019198c450
Benson Mwangi <kimaniben@gmail.com>
2023-12-06 01:27:16 -0500

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,18 @@ import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([])
 
+  const handlesAddQuestion = ((newQuestion) =>{
+    setQuestions([...questions, newQuestion])
+    setPage("List")
+  })
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? 
+        <QuestionForm onAddQuestion={handlesAddQuestion}/> : 
+        <QuestionList questions={questions}/>}
     </main>
   );
 }
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,23 +1,41 @@
-import React from "react";
 
-function QuestionItem({ question }) {
+import React, { useState } from "react";
+function QuestionItem({ question, onDelete, onUpdateCorrectAsw }) {
   const { id, prompt, answers, correctIndex } = question;
-
+  const [selectedCorrectIndex, setSelectedCorrectIndex] = useState(
+    correctIndex
+  );
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
       {answer}
     </option>
   ));
+  const handleUpdateAnswer = () => {
+    onUpdateCorrectAsw(id, selectedCorrectIndex);
+  };
 
+  const handleCorrectIndexChange = (event) => {
+    setSelectedCorrectIndex(parseInt(event.target.value, 10));
+  };
+
+  const handleDelete = () => {
+    onDelete(id);
+  };
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select
+          defaultValue={selectedCorrectIndex}
+          onChange={handleCorrectIndexChange}
+        >
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
+      <button onClick={handleUpdateAnswer}>Update Answer</button>
     </li>
   );
 }
